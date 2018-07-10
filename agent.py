87ee2e38599524255339b3b1417b4dab12d0ea05
antkve <antkve@github.com>
2018-07-10 21:59:42 +0100

    Added event visualization

--- agent.py
+++ agent.py
@@ -16,13 +16,9 @@ class Agent:
                 portfolio_attributes, var_limit) = env.reset()
         self.asset_ixs = range(len(asset_attributes['Return']))
         self.asset_histories = [[] for a in self.asset_ixs]
+        self.last_tradable_events = tradable_events
         self.null_action = np.zeros(len(asset_attributes['Return']))
 
-
-# List of lists of dicts (phew)
-# Each first-level list is an individual asset
-# Each second-level list is a history of timesteps
-# Each dict (timestep) has asset attributes as keys
     def __update_histories(self, assets_attributes):
         self.asset_histories = [asset_history + [timestep] 
                 for asset_history, timestep 
@@ -35,10 +31,20 @@ class Agent:
                 lookahead_events_calendar, tradable_assets,
                 asset_attributes, exposures, 
                 portfolio_attributes, var_limit) = observation
-
+        next_tradable_events = lookahead_events_calendar[1][1]
         self.__update_histories(
-                [{'Level':asset_attributes['Level'][a]
-                    } for a in self.asset_ixs])
+            [
+                dict({'Level':asset_attributes['Level'][a]
+                    },
+                        **{'event_{}'.format(e):\
+                                asset_attributes['Level'][a] 
+                            if tradable_events[e] 
+                            and self.last_tradable_events[e] 
+                            and next_tradable_events[e] 
+                            else None
+                            for e in range(len(tradable_events))})
+                    for a in self.asset_ixs])
+        self.last_tradable_events = tradable_events
         return done
 
     def close(self):
@@ -54,8 +60,7 @@ def ema(ls, T):
         ema = ema + a * (pt - ema)
 
 def cusum_filter(df, h, asset_attribute='Level'):
-    df['ema'] = df.ewm(span=10).mean()
-    print(df['ema'])
+    df['ema'] = df[asset_attribute].ewm(span=10).mean()
     S_pos = S_neg = 0
     filter_points = []
     last_ema = df['ema'][0]
@@ -69,17 +74,14 @@ def cusum_filter(df, h, asset_attribute='Level'):
             pt = None
         filter_points.append(pt)
         last_ema = row['ema']
-    df['filter_points'] = pandas.Series(filter_points)
-    return df
+    return pandas.Series(filter_points)
 
 
 def plot_df(df, x_col=None, assets=[0, 1], colours=None):
-    print(df)
     x_col = x_col or df.index
-    print(x_col)
     colours = colours or ['b' for colname in df.columns]
     for colname, colour in zip(df.columns, colours):
-        if colname != x_col.name:
+        if colname != x_col.name and colour:
             plt.plot(x_col, 
                     df[colname], 
                     colour)
@@ -91,10 +93,11 @@ def visualize_cusum(agent):
     done = False
     while not done:
         done = agent.act()
-    dfs = agent.close()[5:]
+    dfs = agent.close()[4:]
     for ix in range(len(dfs)):
-        dfs[ix]['cusum_sample'] = cusum_filter(dfs[ix], 10)['filter_points']
-        plot_df(dfs[ix], colours=['b', 'y-', 'g^'])
+        dfs[ix]['cusum_sample'] = cusum_filter(dfs[ix], 7.5)
+        plot_df(dfs[ix], colours=[
+            'b', 'ro', 'yo', 'co', 'go', 'y--', 'k^'])
            
 
 env = gym.make('seasonals-v1')
